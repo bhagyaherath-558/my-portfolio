c1f30b692fdf5f7c97118192a1f4d33941b49c5f
bhagya <bhagyaherath558@gmail.com>
2026-06-11 11:34:20 +0530

    complete system fix

--- Downloads/my-portfolio/src/App.js
+++ Downloads/my-portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 import './index.css';
